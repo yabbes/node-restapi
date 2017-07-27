81d3de2f69c6d78550f5793029c806f40abec3cb
Tobias Machulski <yabbaspost@gmail.com>
2017-07-27 09:18:38 +0200

    Working first version of this RESTful API with Mongo DB

---
+++ routes/api.js
@@ -0,0 +1,13 @@
+// dependencies
+var express = require('express');
+var router = express.Router();
+
+// Models
+var Product = require('../models/product');
+
+// routes
+Product.methods(['get', 'put', 'post', 'delete']);
+Product.register(router, '/products');
+
+// Return Router
+module.exports = router;
